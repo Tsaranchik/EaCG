98bddb32f8fb224f92fd97b806034707173c9a62
Владимир Ведин <vovavedin123@gmail.com>
2024-05-17 12:42:27 +0400

    Lab4: Теперь bmp файл читается как бинарник

--- Lab4/src/BMPViewer.java
+++ Lab4/src/BMPViewer.java
@@ -1,66 +1,137 @@
-import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.net.URL;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 import java.util.ArrayList;
-import java.util.Objects;
+import java.util.List;
 
-public class Main {
-    static ArrayList<File> files = new ArrayList<>();
-    static int currImageIndex = 0;
 
-    public static void main(String[] args) throws IOException {
-        File folder = new File("bmp_pics");
-        File[] listOfFiles = folder.listFiles();
+public class BMPViewer extends JPanel {
+    private int width;
+    private int height;
+    private int[] pixels;
+    private List<String> filePaths;
+    private int currentImageIndex;
 
-        for (File file : Objects.requireNonNull(listOfFiles)) {
-            if (file.isFile() && file.getName().endsWith(".bmp"))
-                files.add(file);
+    public BMPViewer(List<String> filePaths) {
+        this.filePaths = filePaths;
+        this.currentImageIndex = 0;
+        loadImage(filePaths.get(currentImageIndex));
+    }
+
+    private void loadImage(String filePath) {
+        try {
+            readBMP(filePath);
+            repaint();
+        } catch (Exception e) {
+            e.printStackTrace();
+            JOptionPane.showMessageDialog(
+                    this,
+                    "Failed to load the BMP file.",
+                    "Error",
+                    JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        if (pixels != null) {
+            for (int y = 0; y < height; ++y) {
+                for (int x = 0; x < width; ++x) {
+                    g.setColor(new Color(pixels[y * width + x]));
+                    g.fillRect(x, y, 1, 1);
+                }
+            }
+        }
+    }
+
+    private void readBMP(String filePath) throws IOException {
+        File file = new File(filePath);
+        FileInputStream fis = new FileInputStream(file);
+
+        byte[] header = new byte[54];
+        fis.read(header);
+
+        if (header[0] != 'B' || header[1] != 'M') {
+            fis.close();
+            throw new IOException("Invalid BMP file.");
         }
 
-        JFrame frame = new JFrame();
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        width = ByteBuffer.wrap(header, 18, 4).order(ByteOrder.LITTLE_ENDIAN).getInt();
+        height = ByteBuffer.wrap(header, 22, 4).order(ByteOrder.LITTLE_ENDIAN).getInt();
 
-        BufferedImage bufferedImage = ImageIO.read(files.get(currImageIndex));
-        frame.setTitle(files.get(currImageIndex).getName());
-        JLabel label = new JLabel(new ImageIcon(bufferedImage));
-        frame.add(label, BorderLayout.CENTER);
+        int offset = ByteBuffer.wrap(header, 10, 4).order(ByteOrder.LITTLE_ENDIAN).getInt();
+        int rowSize = ((width * 3 + 3) / 4) * 4;
 
-        JButton nextButton = new JButton("Next");
-        nextButton.addActionListener(e -> {
-            currImageIndex++;
-            if (currImageIndex >= files.size())
-                currImageIndex = 0;
+        fis.skip(offset - 54);
+        byte[] pixelData = new byte[rowSize * height];
+        fis.read(pixelData);
+        fis.close();
 
-            try {
-                label.setIcon(new ImageIcon(ImageIO.read(files.get(currImageIndex))));
-                frame.setTitle(files.get(currImageIndex).getName());
-            } catch (IOException ioException) {
-                ioException.printStackTrace();
+        pixels = new int[width * height];
+        for (int y = 0; y < height; ++y) {
+            for (int x = 0; x < width; ++x) {
+                int pixelIndex = y * rowSize + x * 3;
+                int blue = pixelData[pixelIndex] & 0xFF;
+                int green = pixelData[pixelIndex + 1] & 0xFF;
+                int red = pixelData[pixelIndex + 2] & 0xFF;
+                int rgb = (red << 16) | (green << 8) | blue;
+                pixels[(height - y - 1) * width + x] = rgb;
             }
-        });
-
-        JButton prevButton = new JButton("Previous");
-        prevButton.addActionListener(e -> {
-            currImageIndex--;
-            if (currImageIndex < 0)
-                currImageIndex = files.size() - 1;
-
-            try {
-                label.setIcon(new ImageIcon(ImageIO.read(files.get(currImageIndex))));
-                frame.setTitle(files.get(currImageIndex).getName());
-            } catch (IOException ioException) {
-                ioException.printStackTrace();
-            }
-        });
-
-        frame.add(nextButton, BorderLayout.EAST);
-        frame.add(prevButton, BorderLayout.WEST);
-
-        frame.setVisible(true);
+        }
     }
+
+    public void nextImage() {
+        if (filePaths.size() > 1) {
+            currentImageIndex = (currentImageIndex + 1) % filePaths.size();
+            loadImage(filePaths.get(currentImageIndex));
+        }
+    }
+
+    public void previousImage() {
+        if (filePaths.size() > 1) {
+            currentImageIndex = (currentImageIndex - 1 + filePaths.size()) % filePaths.size();
+            loadImage(filePaths.get(currentImageIndex));
+        }
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            JFrame frame = new JFrame("BMP Viewer");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setSize(800, 600);
+
+            String folderPath = "/home/tsaran/Desktop/Documents/University/SecondCourse/SecondTerm/EaCG/Lab4/bmp_pics";
+            File folder = new File(folderPath);
+            File[] files = folder.listFiles((dir, name) -> name.toLowerCase().endsWith(".bmp"));
+            List<String> filePaths = new ArrayList<>();
+
+            if (files != null) {
+                for (File file : files) {
+                    filePaths.add(file.getAbsolutePath());
+                }
+            }
+
+            BMPViewer bmpViewer = new BMPViewer(filePaths);
+            frame.add(bmpViewer, BorderLayout.CENTER);
+
+            JPanel buttonPanel = new JPanel();
+            JButton previousButton = new JButton("Previous");
+            JButton nextButton = new JButton("Next");
+
+            previousButton.addActionListener(e -> bmpViewer.previousImage());
+            nextButton.addActionListener(e -> bmpViewer.nextImage());
+
+            buttonPanel.add(previousButton);
+            buttonPanel.add(nextButton);
+
+            frame.add(buttonPanel, BorderLayout.SOUTH);
+            frame.setVisible(true);
+        });
+    }
+
 }
