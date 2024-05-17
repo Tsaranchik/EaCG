74cd19dffe852244f1b13404d0fc2e2cd0bfff64
Владимир Ведин <vovavedin123@gmail.com>
2024-05-17 12:41:31 +0400

    Lab3: Теперь фигура рисуется по собственным координатам и вращается относительно центра координат

--- Lab3/src/Parallelogram.java
+++ Lab3/src/Parallelogram.java
@@ -1,78 +1,199 @@
+import javax.swing.*;
 import java.awt.*;
 
-public class Parallelogram {
-    private Point[] originalVertices;
-    private Point[] vertices;
-    private double rotatingAngle = 0.0;
-    private double scaleFactor = 1.0;
+public class Parallelogram extends JPanel {
+    private double leftTopX = 0;
+    private double leftTopY = 0;
+    private final double baseLength = 3;
+    private final double baseHeight = 3;
+    private final double angle = 20;
+    private double scale = 1.0;
+    private double rotationAngle = 0;
 
-    public Parallelogram(Point topLeft) {
-        this.originalVertices = new Point[4];
-        this.vertices = new Point[4];
-        this.originalVertices[0] = topLeft;
-        this.originalVertices[1] = new Point(topLeft.x + 100, topLeft.y);
-        this.originalVertices[2] = new Point(topLeft.x + 115, topLeft.y + 50);
-        this.originalVertices[3] = new Point(topLeft.x + 15, topLeft.y + 50);
-        resetVertices();
+    public Parallelogram() {
+        setLayout(null);
+        createInputFields();
     }
 
-    public Point[] getVertices() {
-        return vertices;
+    private void createInputFields() {
+        JLabel xLabel =  new JLabel("X:");
+        xLabel.setBounds(10, 10, 50, 25);
+        add(xLabel);
+
+        JTextField xField = new JTextField(String.valueOf(leftTopX));
+        xField.setBounds(70, 10, 50, 25);
+        add(xField);
+
+        JLabel yLabel =  new JLabel("Y:");
+        yLabel.setBounds(10, 40, 50, 25);
+        add(yLabel);
+
+        JTextField yField = new JTextField(String.valueOf(leftTopY));
+        yField.setBounds(70, 40, 50, 25);
+        add(yField);
+
+        JLabel scaleLabel =  new JLabel("Scale:");
+        scaleLabel.setBounds(10, 70, 50, 25);
+        add(scaleLabel);
+
+        JLabel rotationLabel =  new JLabel("Rotation (in degrees):");
+        rotationLabel.setBounds(10, 100, 150, 25);
+        add(rotationLabel);
+
+        JTextField rotationField = new JTextField(String.valueOf(rotationAngle));
+        rotationField.setBounds(160, 100, 50, 25);
+        add(rotationField);
+
+        JTextField scaleField = new JTextField(String.valueOf(scale));
+        scaleField.setBounds(70, 70, 50, 25);
+        add(scaleField);
+
+        JButton drawButton = new JButton("Draw");
+        drawButton.setBounds(10, 130, 110, 25);
+        add(drawButton);
+
+        JButton scaleButton = new JButton("Scale");
+        scaleButton.setBounds(10, 160, 110, 25);
+        add(scaleButton);
+
+        JButton rotateButton = new JButton("Rotate");
+        rotateButton.setBounds(10, 190, 130, 25);
+        add(rotateButton);
+
+        drawButton.addActionListener(e -> {
+            try {
+                leftTopX = Double.parseDouble(xField.getText());
+                leftTopY = Double.parseDouble(yField.getText());
+                repaint();
+            } catch (NumberFormatException ex) {
+                JOptionPane.showMessageDialog(null, "Please enter valid numbers");
+
+            }
+        });
+
+        scaleButton.addActionListener(e -> {
+            try {
+                scale = Double.parseDouble(scaleField.getText());
+                repaint();
+            } catch (NumberFormatException ex) {
+                JOptionPane.showMessageDialog(null, "Please enter valid scale factor");
+
+            }
+        });
+
+        rotateButton.addActionListener(e -> {
+            try {
+                rotationAngle = Double.parseDouble(rotationField.getText());
+                repaint();
+            } catch (NumberFormatException ex) {
+                JOptionPane.showMessageDialog(null, "Please enter valid rotation angle");
+
+            }
+        });
     }
 
-    public Point getCenter() {
-        int x = 0;
-        int y = 0;
-        for (Point vertex : vertices) {
-            x += vertex.x;
-            y += vertex.y;
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2d = (Graphics2D) g;
+
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        int width = screenSize.width;
+        int height = screenSize.height;
+
+        int centerX = width / 2;
+        int centerY = height / 2;
+
+        g2d.drawLine(centerX, 0, centerX, height);
+        g2d.drawLine(0, centerY, width, centerY);
+
+        drawAxisLabels(g2d, centerX, centerY, width, height);
+
+        int x1 = centerX + cmToPixels(leftTopX);
+        int y1 = centerY - cmToPixels(leftTopY);
+        int x2 = x1 + cmToPixels(baseLength);
+        int y2 = y1;
+        int x3 = (int) (x2 + cmToPixels(baseHeight) * Math.tan(Math.toRadians(angle)));
+        int y3 = y2 + cmToPixels(baseHeight);
+        int x4 = (int) (x1 + cmToPixels(baseHeight) * Math.tan(Math.toRadians(angle)));
+        int y4 = y1 + cmToPixels(baseHeight);
+
+        int centerXParallelogram = (x1 + x3) / 2;
+        int centerYParallelogram = (y1 + y3) / 2;
+
+        x1 = scaleCoordinate(x1, centerXParallelogram);
+        y1 = scaleCoordinate(y1, centerYParallelogram);
+        x2 = scaleCoordinate(x2, centerXParallelogram);
+        y2 = scaleCoordinate(y2, centerYParallelogram);
+        x3 = scaleCoordinate(x3, centerXParallelogram);
+        y3 = scaleCoordinate(y3, centerYParallelogram);
+        x4 = scaleCoordinate(x4, centerXParallelogram);
+        y4 = scaleCoordinate(y4, centerYParallelogram);
+
+        int[] xPoints = {x1, x2, x3, x4};
+        int[] yPoints = {y1, y2, y3, y4};
+        rotateParallelogram(xPoints, yPoints, rotationAngle, centerX, centerY);
+
+        g2d.setColor(Color.RED);
+        g2d.drawPolygon(xPoints, yPoints, 4);
+    }
+
+    private int scaleCoordinate(int coord, int centerCoord) {
+        return (int) ((coord - centerCoord) * scale + centerCoord);
+    }
+
+    private void rotateParallelogram(int[] xPoints, int[] yPoints, double angleDegrees, int centerX, int centerY) {
+        double angleRadians = Math.toRadians(angleDegrees);
+        double cosTheta = Math.cos(angleRadians);
+        double sinTheta = Math.sin(angleRadians);
+
+        for (int i = 0; i < xPoints.length; ++i) {
+            int x = xPoints[i] - centerX;
+            int y = yPoints[i] - centerY;
+
+            xPoints[i] = (int) (x * cosTheta - y * sinTheta + centerX);
+            yPoints[i] = (int) (x * sinTheta + y * cosTheta + centerY);
         }
-        return new Point(x / vertices.length, y / vertices.length);
     }
 
-    public void scale(double scaleFactor) {
-        this.scaleFactor = scaleFactor;
-        resetVertices();
-        applyTransformations();
-    }
+    private void drawAxisLabels(Graphics2D g2d, int centerX, int centerY, int width, int height) {
+        int cmInPixels = cmToPixels(1);
+        g2d.setFont(new Font("Arial", Font.PLAIN, 12));
 
-    public void rotate(double angle) {
-        this.rotatingAngle += angle;
-        resetVertices();
-        applyTransformations();
-    }
+        for (int i = cmInPixels; i < width / 2; i += cmInPixels) {
+            g2d.drawLine(centerX + i, centerY - 5, centerX + i, centerY + 5);
+            g2d.drawString(String.valueOf(i / cmInPixels), centerX + i - 10, centerY + 20);
 
-    private void resetVertices() {
-        for (int i = 0; i < vertices.length; ++i)
-            vertices[i] = new Point(originalVertices[i]);
-    }
+            g2d.drawLine(centerX - i, centerY - 5, centerX - i, centerY + 5);
+            g2d.drawString(String.valueOf(-i / cmInPixels), centerX - i - 10, centerY + 20);
+        }
 
-    private void applyTransformations() {
-        Point center = getCenter();
-        double sin = Math.sin(rotatingAngle);
-        double cos = Math.cos(rotatingAngle);
+        for (int i = cmInPixels; i < height / 2; i += cmInPixels) {
+            g2d.drawLine(centerX - 5, centerY - i, centerX + 5, centerY - i);
+            g2d.drawString(String.valueOf(i / cmInPixels), centerX + 10, centerY - i + 5);
 
-        for (Point vertex : vertices) {
-            int dx = vertex.x - center.x;
-            int dy = vertex.y - center.y;
-            vertex.x = center.x + (int) ((dx * cos - dy * sin) * scaleFactor);
-            vertex.y = center.y + (int) ((dx * sin + dy * cos) * scaleFactor);
+            g2d.drawLine(centerX - 5, centerY + i, centerX + 5, centerY + i);
+            g2d.drawString(String.valueOf(-i / cmInPixels), centerX + 10, centerY + i + 5);
         }
     }
 
-    public Point getTopLeft() {
-        return originalVertices[0];
+    public double pixelsToCm(int pixels) {
+        double cmPerPixel = 1.0 / 40;
+        return pixels * cmPerPixel;
     }
 
-    public void setTopLeft(Point topLeft) {
-        int dx = topLeft.x - this.originalVertices[0].x;
-        int dy = topLeft.y - this.originalVertices[0].y;
+    public int cmToPixels(double cm) {
+        double pixelsPerCm = 40;
+        return (int) (cm * pixelsPerCm);
+    }
 
-        for (int i = 0; i < originalVertices.length; ++i) {
-            originalVertices[i] = new Point(originalVertices[i].x + dx, originalVertices[i].y + dy);
-        }
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Parallelogram");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        resetVertices();
-        applyTransformations();
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        frame.setSize(screenSize.width, screenSize.height);
+        frame.add(new Parallelogram());
+        frame.setVisible(true);
     }
 }
